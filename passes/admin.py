37660947fdc619acde55e8e0138d8c823deea03c
Northwest3246 <chelsea005@mail.ru>
2025-09-19 23:45:58 +0200

    feat: register models in Django admin with moderation actions (SQLite)

--- passes/admin.py
+++ passes/admin.py
@@ -1,3 +1,36 @@
 from django.contrib import admin
+from .models import User, Coordinates, Level, Pass, Image
 
-# Register your models here.
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ['email', 'fam', 'name', 'phone']
+    search_fields = ['email', 'fam', 'name']
+
+@admin.register(Coordinates)
+class CoordinatesAdmin(admin.ModelAdmin):
+    list_display = ['latitude', 'longitude', 'height']
+
+@admin.register(Level)
+class LevelAdmin(admin.ModelAdmin):
+    list_display = ['winter', 'summer', 'autumn', 'spring']
+
+@admin.register(Pass)
+class PassAdmin(admin.ModelAdmin):
+    list_display = ['title', 'status', 'add_time', 'user']
+    list_filter = ['status']
+    search_fields = ['title', 'user__email']
+    actions = ['accept_passes', 'reject_passes']
+
+    def accept_passes(self, request, queryset):
+        queryset.update(status='accepted')
+    accept_passes.short_description = "Принять выбранные перевалы"
+
+    def reject_passes(self, request, queryset):
+        queryset.update(status='rejected')
+    reject_passes.short_description = "Отклонить выбранные перевалы"
+
+@admin.register(Image)
+class ImageAdmin(admin.ModelAdmin):
+    list_display = ['pass_object', 'title']
+
+
